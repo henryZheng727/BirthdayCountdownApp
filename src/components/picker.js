ad25a6f28fc0d51cb9b4845426d5127009e7bafd
henryZheng727 <henry.zheng727@gmail.com>
2018-08-03 11:13:26 -0600

    styled picker

--- src/components/picker.js
+++ src/components/picker.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
 
+import 'react-datepicker/dist/react-datepicker.css';
+
 class Picker extends Component {
 
 
@@ -29,7 +31,7 @@ class Picker extends Component {
                     onchange={this.handleChange}
                 />
             </div>
-        )
+        );
     }
 }
 
